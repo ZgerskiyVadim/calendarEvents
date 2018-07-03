42cde7d631ce00d83bf17699eb9296221ed5a549
ZgerskiyVadim <zgerskiy.vadim@mail.ru>
2018-07-03 22:58:20 +0300

    Refactoring

--- modules/runCallbackBeforeEvent.js
+++ modules/runCallbackBeforeEvent.js
@@ -1,5 +1,15 @@
 (function (calendarEvents) {
 
+    calendarEvents.addFuncForEvent(CHANGE_EVENT, (id) => {
+        const changedEvent = calendarEvents.getEvents.filter(event => event.id === id)[0];
+        calendarEvents.getEvents
+            .filter(event => event.parentEventID === id)
+            .forEach(callback => {
+                const eventDate = getNewDateBeforeEvent(changedEvent.newDate, callback.secondsBeforeEvent);
+                calendarEvents.changeEvent(callback.id, changedEvent.eventName, eventDate);
+            });
+    });
+
     calendarEvents.addFuncForEvent(DELETE_EVENT, (id) => {
         calendarEvents.getEvents
             .filter(event => event.parentEventID === id)
@@ -20,18 +30,6 @@
         };
     }
 
-    // Изменить этот приватный метод, добавить к каждому ивенту(до ивента) свойтсво с количеством секунд до вызова основного ивента.
-    // function ifChangeEventAddCallbackBeforeEvent(secondsBeforeEvent) {
-    //     calendarEvents.addFuncForEvent(CHANGE_EVENT, (id) => {
-    //         const events = helperModule.handleEventsPending(calendarEvents.getEvents).filter(event => (event.isCallbackBeforeEvent || event.isCallbackBeforeEventById) && event.id === id);
-    //         events.forEach(event => {
-    //             const beforeEventCallback = helperModule.handleEventsPending(calendarEvents.getEvents).filter(elem => elem.parentEventID === event.id)[0];
-    //             const eventDate = getNewDateBeforeEvent(event.newDate, secondsBeforeEvent);
-    //             calendarEvents.changeEvent(beforeEventCallback.id, event.eventName, eventDate);
-    //         });
-    //     });
-    // }
-
     function changeSecondsCallbackBeforeEvents(secondsBeforeEvent, callback) {
         helperModule.handleEventsPending(calendarEvents.getEvents)
             .filter(event => !event.parentEventID)
@@ -42,13 +40,14 @@
                     calendarEvents.changeEvent(beforeEventCallback.id, beforeEventCallback.eventName, eventDate);
                 } else {
                     event.isCallbackBeforeEvent = true;
-                    const newEvent = addCallbackBeforeEvent(event, secondsBeforeEvent, callback);
+                    const newEvent = createCallbackBeforeEvent(event, secondsBeforeEvent, callback);
+                    newEvent.secondsBeforeEvent = secondsBeforeEvent;
                     newEvent.parentEventID = event.id;
                 }
             });
     }
 
-    function addCallbackBeforeEvent(event, secondsBeforeEvent, callback) {
+    function createCallbackBeforeEvent(event, secondsBeforeEvent, callback) {
         const eventDate = getNewDateBeforeEvent(event.newDate, secondsBeforeEvent);
         return calendarEvents.addNewEvent(event.eventName, eventDate, callback);
     }
@@ -58,7 +57,6 @@
         if (!validationService.isFunction(callback)) return;
 
         changeSecondsCallbackBeforeEvents(secondsBeforeEvent, callback);
-        // ifChangeEventAddCallbackBeforeEvent(secondsBeforeEvent);
     };
 
     calendarEvents.addFuncBeforeEventById = function(id, secondsBeforeEvent, callback) {
@@ -68,8 +66,9 @@
         const event = helperModule.handleEventsPending(calendarEvents.getEvents).filter(event => !event.parentEventID && event.id === id)[0];
         if (event) {
             event.isCallbackBeforeEventById = true;
-            const newEvent = addCallbackBeforeEvent(event, secondsBeforeEvent, callback);
+            const newEvent = createCallbackBeforeEvent(event, secondsBeforeEvent, callback);
             newEvent.isCreatedByEventID = true;
+            newEvent.secondsBeforeEvent = secondsBeforeEvent;
             newEvent.parentEventID = event.id;
         }
     };
