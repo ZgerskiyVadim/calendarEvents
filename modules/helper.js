abb4b86ccfd65f930494e3ec6aebf9d2479f9122
ZgerskiyVadim <zgerskiy.vadim@mail.ru>
2018-10-04 15:10:54 +0300

    Refactoring

--- modules/helper.js
+++ modules/helper.js
@@ -59,9 +59,7 @@ const helperModule = (function () {
         },
 
         generateId() {
-            return Math.floor((1 + Math.random()) * 0x10000)
-                .toString(16)
-                .substring(1);
+            return Math.random().toString(36).substr(2, 4);
         }
 
     };
